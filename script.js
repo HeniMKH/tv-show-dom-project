f4f124b15de7148f4e83993359b03c40eafeaef6
Hengameh M.Khani <108895187+HeniMKH@users.noreply.github.com>
2023-02-01 20:44:31 +0000

    my name

--- script.js
+++ script.js
@@ -10,3 +10,35 @@ function makePageForEpisodes(episodeList) {
 }
 
 window.onload = setup;
+
+function makePageForEpisodes(episodeList) {
+  const rootElem = document.getElementById("root");
+  episodeList.forEach((episode) => {
+    const episodeContainer = document.createElement("div");
+    episodeContainer.className = "episode";
+
+    const episodeName = document.createElement("h2");
+    episodeName.textContent = episode.name;
+    episodeContainer.appendChild(episodeName);
+
+    const episodeCode = document.createElement("h3");
+    episodeCode.textContent = `S${("0" + episode.season).slice(-2)}E${("0" + episode.number).slice(-2)}`;
+    episodeContainer.appendChild(episodeCode);
+
+    const episodeImage = document.createElement("img");
+    episodeImage.src = episode.image.medium;
+    episodeContainer.appendChild(episodeImage);
+
+    const episodeSummary = document.createElement("p");
+    episodeSummary.textContent = episode.summary;
+    episodeContainer.appendChild(episodeSummary);
+
+    rootElem.appendChild(episodeContainer);
+  });
+
+  const link = document.createElement("p");
+  link.innerHTML = `Data originally from <a href="https://www.tvmaze.com/">TVMaze.com</a>`;
+  rootElem.appendChild(link);
+}
+
+window.onload = setup;
